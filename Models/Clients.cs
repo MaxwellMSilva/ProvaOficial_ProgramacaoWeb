b2b954d763abf0335c59850fa691043d3ff34dd8
MaxwellDefault <maxwellmachadom.m.s@gmail.com>
2021-04-16 15:28:10 -0400

    Adicionar arquivos de projeto.

---
+++ Models/Clients.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prova.Models
+{
+    public class Clients
+    {
+        public string nome { get; set; }
+        public string cpf { get; set; }
+        public string sexo { get; set; }
+        public string endereco { get; set; }
+        public int numero { get; set; }
+        public string bairro { get; set; }
+        public string cep { get; set; }
+        public string telefone { get; set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prova.Models
+{
+    public class Produto
+    {
+        public int codigoProduto { get; set; }
+        public double preco { get; set; }
+        public string descricao { get; set; }
+        public string categoria { get; set; }
+        public string observacao { get; set; }
+    }
+}
